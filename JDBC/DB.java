46accda47e5c769ab15a6d155d600a7029e17f47
johndiddles <johnadepelumi@gmail.com>
2025-09-21 19:43:18 +0100

    JDBC: run query using prepared statement

--- JDBC/DB.java
+++ JDBC/DB.java
@@ -4,9 +4,15 @@ public class DB {
     public static void main(String[] args) throws Exception {
         try {
             Class.forName("org.postgresql.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/domeasolid", "postgres", "123456");
-            Statement statement = conn.createStatement();
-            ResultSet result = statement.executeQuery("SELECT * FROM influencer_payments");
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/learnjava", "postgres", "123456");
+
+            String query = "SELECT * FROM influencer_payments WHERE tier = ?";
+
+            PreparedStatement pst = conn.prepareStatement(query);
+            pst.setString(1, "Solid Bronze");
+
+            ResultSet result = pst.executeQuery();
+
             while (result.next()) {
                 System.out.println(result.getString(1) + " " + result.getString(2) + " " + result.getString(3));
             }
